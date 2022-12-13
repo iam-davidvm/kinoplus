4e4ed1d118c664860e3863bf9d98a1186ad83d00
iam-davidvm <vanmelkebekedavid@gmail.com>
2022-12-13 23:41:13 +0100

    Set-up first view

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+const engine = require('ejs-mate');
 
 /* import routes */
 const kinoRoutes = require('./routes/kino');
+
+/* view settings */
+app.engine('ejs', engine);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
+// Needed to use the bulma framework
+app.use('/bulma', express.static(__dirname + '/node_modules/bulma/'));
 
 /* use routes */
 app.use('/kino', kinoRoutes);
