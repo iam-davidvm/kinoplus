8f802aaf936855683eed218bfde4cb86d1bdeaa4
iam-davidvm <vanmelkebekedavid@gmail.com>
2022-12-14 00:59:41 +0100

    changing the seedfile

---
+++ seeds/cinemaHelpers.js
@@ -0,0 +1,90 @@
+module.exports.names = [
+  'Starlight Cinemas',
+  'Silver Screen Theaters',
+  'Cineplex Odeon',
+  'The Grand Theater',
+  'Galaxy Cinemas',
+  'The Royal Cinema',
+  'Skyline Cinemas',
+  'The Majestic Theater',
+  'The Met Cinema',
+  'The Encore Theater',
+];
+
+module.exports.locations = [
+  'Times Square, New York, NY',
+  'Hollywood, Los Angeles, CA',
+  'The Loop, Chicago, IL',
+  'Downtown Houston, TX',
+  'Downtown Phoenix, AZ',
+  'Rittenhouse Square, Philadelphia, PA',
+  'The Alamo, San Antonio, TX',
+  'Gaslamp Quarter, San Diego, CA',
+  'Deep Ellum, Dallas, TX',
+  'Downtown San Jose, CA',
+];
+
+module.exports.descriptions = [
+  'A state-of-the-art multiplex featuring luxury seating and the latest technology.',
+  'A classic, old-fashioned movie theater with a charming atmosphere.',
+  'A popular chain of movie theaters offering a variety of film choices.',
+  'A grand, historic theater showcasing the best in independent and foreign films.',
+  'A futuristic-looking multiplex with 3D screenings and interactive experiences.',
+  'An elegant, upscale cinema with comfortable seating and gourmet concessions.',
+  'A modern multiplex with a variety of film choices and affordable ticket prices.',
+  'A beautiful, art deco theater known for its impressive acoustics and architecture.',
+  'A trendy, hipster-friendly cinema offering a mix of mainstream and arthouse films.',
+  'A high-end theater with plush seating and a wide selection of craft cocktails and snacks.',
+];
+
+module.exports.emails = [
+  'starlightcinemas@gmail.com',
+  'silverscreentheaters@gmail.com',
+  'cineplexodeon@gmail.com',
+  'thegrandtheater@gmail.com',
+  'galaxycinemas@gmail.com',
+  'theroyalcinema@gmail.com',
+  'skylinecinemas@gmail.com',
+  'themajestictheater@gmail.com',
+  'themetcinema@gmail.com',
+  'theencoretheater@gmail.com',
+];
+
+module.exports.phones = [
+  '(212) 555-1234', // Times Square, New York, NY
+  '(323) 555-1234', // Hollywood, Los Angeles, CA
+  '(312) 555-1234', // The Loop, Chicago, IL
+  '(713) 555-1234', // Downtown Houston, TX
+  '(602) 555-1234', // Downtown Phoenix, AZ
+  '(215) 555-1234', // Rittenhouse Square, Philadelphia, PA
+  '(210) 555-1234', // The Alamo, San Antonio, TX
+  '(619) 555-1234', // Gaslamp Quarter, San Diego, CA
+  '(214) 555-1234', // Deep Ellum, Dallas, TX
+  '(408) 555-1234', // Downtown San Jose, CA
+];
+
+module.exports.owners = [
+  'John Smith',
+  'Jane Doe',
+  'Bob Johnson',
+  'Emily Brown',
+  'William Thompson',
+  'Ashley Davis',
+  'Michael Martinez',
+  'Samantha Lee',
+  'Christopher Taylor',
+  'Amanda White',
+];
+
+module.exports.images = [
+  'https://images.unsplash.com/photo-1536440136628-849c177e76a1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1450&q=80',
+  'https://images.unsplash.com/photo-1513015579021-66c7c008af85?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fG1vdmllJTIwdGhlYXRlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60',
+  'https://images.unsplash.com/photo-1563381013529-1c922c80ac8d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1626&q=80',
+  'https://images.unsplash.com/photo-1510827220565-c6a086ff31c8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80',
+  'https://images.unsplash.com/photo-1512113569142-8a60fccc7caa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80',
+  'https://images.unsplash.com/photo-1614460644009-f2343220f276?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDd8fG1vdmllJTIwdGhlYXRlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60',
+  'https://images.unsplash.com/photo-1595617564647-2562faab5fb7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDV8fG1vdmllJTIwdGhlYXRlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60',
+  'https://images.unsplash.com/photo-1620222396988-a84a376ea5ed?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80',
+  'https://images.unsplash.com/photo-1584983162722-6b827b369ac6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
+  'https://images.unsplash.com/photo-1620155235975-3124654fddb0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTAyfHxtb3ZpZSUyMHRoZWF0ZXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
+];
--- seeds/index.js
+++ seeds/index.js
@@ -1,6 +1,14 @@
 const mongoose = require('mongoose');
-const { db } = require('../models/cinema');
 const Cinema = require('../models/cinema');
+const {
+  names,
+  locations,
+  descriptions,
+  emails,
+  phones,
+  owners,
+  images,
+} = require('./cinemaHelpers');
 
 mongoose
   .connect('mongodb://localhost:27017/kinoplus')
@@ -18,50 +26,18 @@ const seedDb = async () => {
   await Cinema.deleteMany({});
 
   /* better to use a for loop */
-  const cinema1 = new Cinema({
-    name: 'Cinema Central',
-    location: 'Ninove',
-    description: 'Little, old and scruffy cinema.',
-    image:
-      'https://images.unsplash.com/photo-1536440136628-849c177e76a1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1450&q=80',
-    owner: 'Tania De Haeseleir',
-    email: 'hello@cinema-central.be',
-    phone: '+3254223344',
-  });
-  const cinema2 = new Cinema({
-    name: 'Palace Aalst',
-    location: 'Aalst',
-    description: 'Movie theater with not a lot of attendees.',
-    image:
-      'https://images.unsplash.com/photo-1513015579021-66c7c008af85?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fG1vdmllJTIwdGhlYXRlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60',
-    owner: 'Alexander Tournedos',
-    email: 'info@palace-aalst.be',
-    phone: '+3253448899',
-  });
-  const cinema3 = new Cinema({
-    name: 'Bioscoop Focus',
-    location: 'Geraardsbergen',
-    description: 'Focus on movies',
-    image:
-      'https://images.unsplash.com/photo-1563381013529-1c922c80ac8d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1626&q=80',
-    owner: 'Guido Zebra',
-    email: 'guido@focus.be',
-    phone: '+3254873244',
-  });
-  const cinema4 = new Cinema({
-    name: 'Kinepolis',
-    location: 'Brussel',
-    description: 'Near the Atomium',
-    image:
-      'https://images.unsplash.com/photo-1510827220565-c6a086ff31c8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80',
-    owner: 'Cathérine Dujardin',
-    email: 'sales@kinepolis.be',
-    phone: '+3225483244',
-  });
-  await cinema1.save();
-  await cinema2.save();
-  await cinema3.save();
-  await cinema4.save();
+  for (let i = 0; i < 10; i++) {
+    const cinema = new Cinema({
+      name: names[i],
+      location: locations[i],
+      description: descriptions[i],
+      image: images[i],
+      owner: owners[i],
+      email: emails[i],
+      phone: phones[i],
+    });
+    await cinema.save();
+  }
 };
 
 seedDb().then(() => {
